0445e8eb6b4be6ac0b79195ada34db8d5d7e1b2f
Enilton Rodrigues de Andrade <erodri@br.ibm.com>
2024-08-16 15:06:40 -0300

    (feat): creates auth pages

---
+++ apps/web/src/app/auth/layout.tsx
@@ -0,0 +1,14 @@
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div
+      lang="en"
+      className="flex min-h-screen flex-col items-center justify-center px-4"
+    >
+      <div className="w-full max-w-xs">{children}</div>
+    </div>
+  )
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>New</h1>
+  return <Button>New</Button>
 }
