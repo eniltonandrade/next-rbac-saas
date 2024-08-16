74af0e79e0fd519cebcb7a5680248004933c73e4
Enilton Rodrigues de Andrade <erodri@br.ibm.com>
2024-08-16 18:01:11 -0300

    (feat): creates auth forms

---
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -0,0 +1,61 @@
+'use server'
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
+const signInSchema = z.object({
+  email: z.string().email({
+    message: 'Please, provide a valid e-mail address',
+  }),
+  password: z.string().min(1, {
+    message: 'Please, provide your password',
+  }),
+})
+
+export async function signInWithEmailAndPassword(_: unknown, data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return {
+      success: false,
+      message: null,
+      errors,
+    }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+    console.log(token)
+  } catch (err) {
+    // console.log(err)
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json<{ message: string }>()
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    console.error(err)
+
+    return {
+      success: true,
+      message: 'Unexpected Error, try again later',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: null,
+    errors: null,
+  }
+}
